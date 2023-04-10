04c1b384bc449431e0fff4236825b116689f28f1
Diman331 <diman33133@gmail.com>
2023-04-10 15:45:00 +0700

    Update 2.py

--- Seminar_2/2.py
+++ Seminar_2/2.py
@@ -1,2 +1,10 @@
 # Задача 2. Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z.
 
+print("| X | Y | Z | ¬(X ∧ Y) ∨ Z |")
+print("|---|---|---|--------------|")
+
+for X in range(2):
+    for Y in range(2):
+        for Z in range(2):
+            result = not(X and Y) or Z
+            print(f"| {X} | {Y} | {Z} |      {int(result)}       |")
